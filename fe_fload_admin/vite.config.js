30de1268f8184eff0becbf802652082e61b87a0e
Phong <phongpvse161146@fpt.edu.vn>
2026-03-04 00:14:10 +0700

    fixx

--- fe_fload_admin/vite.config.js
+++ fe_fload_admin/vite.config.js
@@ -1,10 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "./", // 👈 thêm dòng này cho production
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
